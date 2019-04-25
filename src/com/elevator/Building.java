14508e7beae5ad277062a8d005d84c9e08496dcc
一坐小城 <774463266@qq.com>
2019-04-25 10:52:49 +0800

    @mod 电梯转向问题

--- src/com/elevator/Building.java
+++ src/com/elevator/Building.java
@@ -11,163 +11,168 @@ import com.elevator.Elevator.RUNNING;
 import com.elevator.Person.STATE;
 
 public class Building {
-	
+
 	/*
 	 * public static final BlockingQueue<Floor> queue= new ArrayBlockingQueue(100);
 	 */
-	
+
 	public static final  List<Floor>  queue=new ArrayList(7);
 	private static void initQueue(){
 		for(int i=0;i<7;i++)
 			queue.add(null);
 	}
-	
+
 	public static volatile Boolean isWork=true;
-	
-  public static class Floor{
-	  
-	  Floor(Integer floorPostion){
-		  this.floorPostion=floorPostion;
-	  }
-	 
-	  // 重写equal
-	 private String uuid=UUID.randomUUID().toString();
-	  // 楼层数字 
-	 public final Integer floorPostion;
-	  // 楼层等电梯的人
-	 public final List<Person> persons=new LinkedList(); 
-	 public static final Integer TOP=6;
-	 public static final Integer BOTTOM=-1;
-	 // 上楼请求
-	 public Boolean upRequest=false;
-	 
-	 // 下楼请求
-	 public Boolean downRequest=false;
-	 
-	 // 通知这个楼层的人电梯到了 可以进了
-	 public void informPerson(Elevator ele) {
-		 List<Person>  enter=new LinkedList(); 
-		 for(Person itm:persons) {
-			 if(itm.shouldEnterElevator(ele))
-				 enter.add(itm);
-		 }
-		 //persons.removeIf(person->{return person.getState()==STATE.INELEVATOR;});
-		 persons.removeAll(enter);
-	 }
-	 
-	 public void addWaitPerson(Person person) {
-		 persons.add(person);
-		 sendRequest(person);
-	 }
-	 
-	 // 坐电梯请求
-	 public void sendRequest(Person person) {
-		 System.out.println("开始处理请求");
-		 if(downRequest&&upRequest)
-			 return ;
-		 if(person.getRunState()!=null) {
-			 if(person.getRunState()==RUNNING.UP&&!upRequest) 
-				 upRequest=true;
-			 else
-				 downRequest=true;
-			 System.out.println("加入请求队列"); 
-		  if(queue.get(this.floorPostion+1)==null)	 
-			    queue.set(this.floorPostion+1, this);  //按照序号存放
-			
-			 // queue.sort((f1,f2)->{return f1.floorPostion-f2.floorPostion;});  //
-		 }
-	 }
-	 
+
+	public static class Floor{
+
+		Floor(Integer floorPostion){
+			this.floorPostion=floorPostion;
+		}
+
+		// 重写equal
+		private String uuid=UUID.randomUUID().toString();
+		// 楼层数字
+		public final Integer floorPostion;
+		// 楼层等电梯的人
+		public final List<Person> persons=new LinkedList();
+		public static final Integer TOP=6;
+		public static final Integer BOTTOM=-1;
+		// 上楼请求
+		public Boolean upRequest=false;
+
+		// 下楼请求
+		public Boolean downRequest=false;
+
+		// 通知这个楼层的人电梯到了 可以进了
+		public void informPerson(Elevator ele) {
+			List<Person>  enter=new LinkedList();
+			for(Person itm:persons) {
+				if(itm.shouldEnterElevator(ele))
+					enter.add(itm);
+			}
+			//persons.removeIf(person->{return person.getState()==STATE.INELEVATOR;});
+			persons.removeAll(enter);
+		}
+
+		public void addWaitPerson(Person person) {
+			persons.add(person);
+			sendRequest(person);
+		}
+
+		// 坐电梯请求
+		public void sendRequest(Person person) {
+			System.out.println("开始处理请求");
+			if(downRequest&&upRequest)
+				return ;
+			if(person.getRunState()!=null) {
+				if(person.getRunState()==RUNNING.UP&&!upRequest)
+					upRequest=true;
+				else
+					downRequest=true;
+				System.out.println("加入请求队列");
+				if(queue.get(this.floorPostion+1)==null)
+					queue.set(this.floorPostion+1, this);  //按照序号存放
+
+				// queue.sort((f1,f2)->{return f1.floorPostion-f2.floorPostion;});  //
+			}
+		}
+
 		public String getUuid() {
 			return uuid;
 		}
-		
-	    @Override
+
+		@Override
 		public boolean equals(Object person) {
-	    	System.out.println("-----------开始equals 方法");
-		    if(person instanceof Floor) {	
-		    	System.out.println("-----------开始equals 方法   "+this.uuid.equals(((Floor)person).getUuid()));
-			   return this.uuid.equals(((Floor)person).getUuid());
-		    }
-		    return false;
+			System.out.println("-----------开始equals 方法");
+			if(person instanceof Floor) {
+				System.out.println("-----------开始equals 方法   "+this.uuid.equals(((Floor)person).getUuid()));
+				return this.uuid.equals(((Floor)person).getUuid());
+			}
+			return false;
 		}
-  }
-  
-  public static Floor BottomFloor=new Floor(-1);
-  public static Floor ZEROFloor=new Floor(0);
-  public static Floor ONEFloor=new Floor(1);
-  public static Floor TWOFloor=new Floor(2);
-  public static Floor THREEFloor=new Floor(3);
-  public static Floor FOURFloor=new Floor(4);
-  public static Floor FIVEFloor=new Floor(5);
- 
-  public static Elevator left=new Elevator(BottomFloor,"left");
-  public static Elevator middle=new Elevator(TWOFloor,"middle");
-  public static Elevator right=new Elevator(FIVEFloor,"right");
-  
-  private static List<Elevator> elevators =new ArrayList();
-  private static  void initElevators(){
-	  elevators.add(left);
-	 // elevators.add(middle);
-	  //elevators.add(right);
-  }
-     
-  
-  private static List<Floor>  floors=new ArrayList();
-  private static void initfloors(){
-	  floors.add(BottomFloor);
-	  floors.add(ZEROFloor);
-	  floors.add(ONEFloor);
-	  floors.add(TWOFloor);
-	  floors.add(THREEFloor);
-	  floors.add(FOURFloor);
-	  floors.add(FIVEFloor);
-	  
-  }
-  
-  public static Floor getFloorByNum(Integer num){
-	if(num<Floor.BOTTOM)  num=Floor.BOTTOM;
-	if(num>Floor.TOP)num=Floor.TOP;
-	return floors.get(num+1);
-  }
-  
-  
-   public static void schdule() {
-	   System.out.println("开始 运行");
-	   initQueue();
-	   initfloors();
-	   initElevators();
-	   
-	   new Thread(()->{
-		for(int i=0;isWork;i=(i+1)%7) {
-			Floor todeal=queue.get(i);
-			if(todeal==null)	
-				continue;
-			System.out.println(String.format("收到去往 %d 楼的请求  index为：%d",todeal.floorPostion,i));
-			dispatch(todeal);
-			
-		}
-	   }).start();
-	   
-   }
-    //分派
+	}
+
+	public static Floor BottomFloor=new Floor(-1);
+	public static Floor ZEROFloor=new Floor(0);
+	public static Floor ONEFloor=new Floor(1);
+	public static Floor TWOFloor=new Floor(2);
+	public static Floor THREEFloor=new Floor(3);
+	public static Floor FOURFloor=new Floor(4);
+	public static Floor FIVEFloor=new Floor(5);
+
+	public static Elevator left=new Elevator(BottomFloor,"left");
+	public static Elevator middle=new Elevator(TWOFloor,"middle");
+	public static Elevator right=new Elevator(FIVEFloor,"right");
+
+	private static List<Elevator> elevators =new ArrayList();
+	private static  void initElevators(){
+		elevators.add(left);
+		// elevators.add(middle);
+		//elevators.add(right);
+	}
+
+
+	private static List<Floor>  floors=new ArrayList();
+	private static void initfloors(){
+		floors.add(BottomFloor);
+		floors.add(ZEROFloor);
+		floors.add(ONEFloor);
+		floors.add(TWOFloor);
+		floors.add(THREEFloor);
+		floors.add(FOURFloor);
+		floors.add(FIVEFloor);
+
+	}
+
+	public static Floor getFloorByNum(Integer num){
+		if(num<Floor.BOTTOM)  num=Floor.BOTTOM;
+		if(num>Floor.TOP)num=Floor.TOP;
+		return floors.get(num+1);
+	}
+
+	public static void initElevatorsStart() {
+		for(Elevator itn:elevators)
+			itn.start();
+		System.out.println("");
+	}
+
+	public static void schdule() {
+		System.out.println("开始 运行");
+		initQueue();
+		initfloors();
+		initElevators();
+		initElevatorsStart();
+		new Thread(()->{
+			for(int i=0;isWork;i=(i+1)%7) {
+				Floor todeal=queue.get(i);
+				if(todeal==null)
+					continue;
+				System.out.println(String.format("收到去往 %d 楼的请求  index为：%d",todeal.floorPostion,i));
+				dispatch(todeal);
+
+			}
+		}).start();
+
+	}
+	//分派
 	private static void dispatch(Floor todeal) {
 		List<Elevator> list= findTheCandidate(todeal);
 		handleRequest(list,todeal);
 	}
 	private static void handleRequest(List<Elevator> list, Floor todeal) {
-		 if(list==null||list.size()==0)
-			 return ;
-		 Elevator target=null;
-		 Integer distance=10;
-		 for(Elevator itm:list) {
-			 int dis=Math.abs(itm.currentFloor.floorPostion-todeal.floorPostion);
-			 if(dis<distance) {
-				 distance=dis;
-				 target=itm;
-			 }
-		 }
-		 target.addStopFloor(todeal);
+		if(list==null||list.size()==0)
+			return ;
+		Elevator target=null;
+		Integer distance=10;
+		for(Elevator itm:list) {
+			int dis=Math.abs(itm.currentFloor.floorPostion-todeal.floorPostion);
+			if(dis<distance) {
+				distance=dis;
+				target=itm;
+			}
+		}
+		target.addStopFloor(todeal);
 		/*
 		 * if((target.running.code&RUNNING.UP.code)>0) { todeal.upRequest=false;
 		 * if(!todeal.downRequest) queue.set(todeal.floorPostion+1,null); }
@@ -178,11 +183,11 @@ public class Building {
 		 * todeal.upRequest=false; queue.set(todeal.floorPostion, null) ; }
 		 * target.addStopFloor(todeal); }
 		 */
-		    System.out.println(String.format("电梯：%s 已经受理该请求", target.name));
+		System.out.println(String.format("电梯：%s 已经受理该请求", target.name));
 	}
-	
-	
-	
+
+
+
 	private static List<Elevator> findTheCandidate(Floor todeal) {
 		List<Elevator> candidate =new LinkedList();
 		// 上楼请求
@@ -203,7 +208,7 @@ public class Building {
 						return candidate;
 					}
 				}
-				else if(ele.running==RUNNING.STILL) {
+				else if(ele.running.code==RUNNING.STILL.code) {
 					if(!todeal.downRequest) {
 						queue.set(todeal.floorPostion+1,null);
 					}
@@ -229,7 +234,7 @@ public class Building {
 						return candidate;
 					}
 				}
-				else if(ele.running==RUNNING.STILL) {
+				else if(ele.running.code==RUNNING.STILL.code) {
 					if(!todeal.upRequest) {
 						queue.set(todeal.floorPostion+1,null);
 					}
@@ -237,11 +242,11 @@ public class Building {
 					candidate.add(ele);
 				}
 			}
-		
+
 		}
-	    return candidate;
+		return candidate;
 	}
-	
+
 	public static void main(String[] args) {
 		List<Integer> list=new LinkedList();
 		list.add(6);
--- src/com/elevator/Elevator.java
+++ src/com/elevator/Elevator.java
@@ -11,44 +11,49 @@ import com.elevator.Building.Floor;
 public class Elevator extends Thread{
 
 	public enum RUNNING{
+		//4       16         0         2               8
 		UP(1<<2),DOWN(1<<4),STILL(0),OPENDOOR(1<<1),CLOSEDOOR(1<<3);
 		public  Integer code;
 		RUNNING(Integer code){
 			this.code=code;
 		}
 	}
-	
-    public RUNNING running;
+
+	// 运行状态 包括门的状态
+	public RUNNING running;
+
+	// 实际的运行方向 只包方向
+	private Integer  runDrirection;
 	/* private BlockingQueue<Floor> toStop= new ArrayBlockingQueue(100); */
-	 
+
 	private  volatile List<Floor>  toStop=new ArrayList(7);
 	/*
 	 * { for(int i=0;i<7;i++) toStop.add(null); }
 	 */
-	
+
 	public  Floor currentFloor;
-	
+
 	public final String name;
-	
+
 	private final List<Person> persons=new ArrayList(3);
-	
+
 	private final Integer Max=3;
-	
+
 	private Integer currentSize=0;
-	
+
 	private String getWork="%s ：我在%d楼  接到了 去%d楼的请求";
 	private String outPerson="%s ：在%d楼下楼去了";
 	private String inPerson="%s ： 在%d楼进电梯了";
 	private String netxstop="%s :下一个要停的楼层是 %d楼层   目前处于 %d楼";
-	
+
 	private volatile  Floor nextToStop;
-	
+
 	public Boolean addInPerson(Person person) {
 		if(currentSize<Max) {
-			 persons.add(person);
-			 currentSize+=1;
-		System.out.println(String.format(inPerson,name,currentFloor.floorPostion));		 
-		return true;
+			persons.add(person);
+			currentSize+=1;
+			System.out.println(String.format(inPerson,name,currentFloor.floorPostion));
+			return true;
 		}
 		return false;
 	}
@@ -56,76 +61,81 @@ public class Elevator extends Thread{
 	public Boolean couldGetIn() {
 		return currentSize<persons.size();
 	}
-	
+
 	public void outPerson(Person person) {
 		// TODO Auto-generated method stub
-		 persons.remove(person);
-		 currentSize-=1;
+		persons.remove(person);
+		currentSize-=1;
 	}
 
 	public void addStopFloor(Floor targetPosition) {
 		//if(!toStop.contains(targetPosition))
-		    toStop.add(targetPosition);
+		toStop.add(targetPosition);
 		//if(toStop.size()>=2) {
-			if((running.code&RUNNING.UP.code)>0) {
-				toStop.sort((a,b)->{return a.floorPostion-b.floorPostion;});
-			}else if((running.code&RUNNING.DOWN.code)>0) {
-				toStop.sort((a,b)->{return b.floorPostion-a.floorPostion;});
-			} 
-			else {
-				toStop.sort((a,b)->{return a.floorPostion-b.floorPostion;});
-			}
+		if((running.code&RUNNING.UP.code)>0) {
+			toStop.sort((a,b)->{return a.floorPostion-b.floorPostion;});
+		}else if((running.code&RUNNING.DOWN.code)>0) {
+			toStop.sort((a,b)->{return b.floorPostion-a.floorPostion;});
+		}
+		else {
+			toStop.sort((a,b)->{return a.floorPostion-b.floorPostion;});
+		}
 		//}
 		System.out.println(String.format(netxstop,name,toStop.get(0).floorPostion,currentFloor.floorPostion));
 	}
-    
-	
+
+
 	private Floor getnextStop() {
 		//synchronized(toStop)  {
 		if(toStop.size()==0) return null;
 		return toStop.get(0);
 		//}
 	}
-	
+
 	private void done() {
 		toStop.remove(0);
 	}
-	
+
 	private void done(Floor currentFloor) {
 		//synchronized(toStop)  {
-			toStop.remove(currentFloor);
+		toStop.remove(currentFloor);
 		//}
 	}
-	
+
 	public void run() {
 		if(this.running==RUNNING.STILL) {
 			while(1==1) {
-			if(getnextStop()!=null) {
-				if(getnextStop().floorPostion>currentFloor.floorPostion) {
-					this.running.code=this.running.code|RUNNING.UP.code;
-					System.out.println(String.format(getWork,name,currentFloor.floorPostion,getnextStop().floorPostion));
-					work();
-					continue;
+				if(getnextStop()!=null) {
+					if(getnextStop().floorPostion>currentFloor.floorPostion) {
+						runDrirection=RUNNING.UP.code;
+						this.running.code=runDrirection;
+						System.out.println(String.format(getWork,name,currentFloor.floorPostion,getnextStop().floorPostion)+" "+this.running.code);
+						System.out.println("准备上楼 "+this.running.code);
+						work();
+						continue;
+					}
+					else if(getnextStop().floorPostion<currentFloor.floorPostion) {
+						runDrirection=RUNNING.DOWN.code;
+						this.running.code=runDrirection;
+						System.out.println(String.format(getWork,name,currentFloor.floorPostion,getnextStop().floorPostion)+" "+this.running.code);
+						System.out.println("准备下楼 "+this.running.code);
+						work();
+						continue;
+					}
+					else {
+						System.out.println(name+":在本层 有请求 直接开门处理");
+						this.running.code=RUNNING.OPENDOOR.code;
+						currentFloor.informPerson(this);
+						done(currentFloor);
+						this.running.code=RUNNING.CLOSEDOOR.code;
+					}
 				}
-				else if(getnextStop().floorPostion<currentFloor.floorPostion) {
-					this.running.code=this.running.code|RUNNING.DOWN.code;
-					System.out.println(String.format(getWork,name,currentFloor.floorPostion,getnextStop().floorPostion));
-					work();
-					continue;
-				}
-				else {
-					 this.running=RUNNING.OPENDOOR;
-					 currentFloor.informPerson(this);
-					 done(currentFloor);
-					 this.running=RUNNING.CLOSEDOOR;
-				}
+				//System.out.println("没有活干 原地待命");
+				runCosumTime();
 			}
-			 //System.out.println("没有活干 原地待命");
-			runCosumTime();
-		 }
 		}
 	}
-	
+
 	// 模拟花费的时间
 	public void runCosumTime() {
 		try {
@@ -135,45 +145,65 @@ public class Elevator extends Thread{
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+
 	// 这里沿着一个方向走 最后停止
 	private void work() {
 		while(getnextStop()!=null) {
-		Integer nextFloor=currentFloor.floorPostion;
-		if((this.running.code&RUNNING.UP.code)>0)
-			nextFloor=nextFloor+1;
-		else if((this.running.code&RUNNING.DOWN.code)>0)
-			nextFloor=nextFloor-1;
-		runCosumTime();
-		currentFloor=Building.getFloorByNum(nextFloor);
-		System.out.println(String.format(netxstop,name,getnextStop().floorPostion,currentFloor.floorPostion));
-		if(getnextStop().floorPostion==currentFloor.floorPostion) {
-			done(currentFloor);
-			this.running.code=this.running.code|RUNNING.OPENDOOR.code;
-			informPerson(); //出
-			currentFloor.informPerson(this); //进
-			this.running.code=this.running.code|RUNNING.CLOSEDOOR.code;
-		  }
+			Integer nextFloor=currentFloor.floorPostion;
+			if((this.running.code&RUNNING.UP.code)>0)
+				nextFloor=nextFloor+1;
+			else if((this.running.code&RUNNING.DOWN.code)>0)
+				nextFloor=nextFloor-1;
+			runCosumTime();
+			currentFloor=Building.getFloorByNum(nextFloor);
+			System.out.println(String.format(netxstop,name,getnextStop().floorPostion,currentFloor.floorPostion));
+			if(getnextStop().floorPostion==currentFloor.floorPostion) {
+				done(currentFloor);
+				this.running.code=runDrirection|RUNNING.OPENDOOR.code;
+				informPerson(); //出
+				currentFloor.informPerson(this); //进
+				// 到打一个楼层后 要重新检查 运行状态 是否可以和原来的保持一致
+				// 人进入电梯后 获取下一个要去的楼层  如果与原来的运行状态一致 那么继续 如果不一致 那么直接跳出
+				this.running.code=runDrirection|RUNNING.CLOSEDOOR.code;
+				if(shouldChangeDirection())
+					break;
+			}
 		}
-		this.running=RUNNING.STILL;
+
+		//this.running=RUNNING.STILL; 这么写不起作用
+
+		this.running.code=0;
+		System.out.println(name+" "+this.running.code);
+	}
+
+	// 到打一层后 检查要去的下一层 是否 符合当前的运行方向
+	private boolean shouldChangeDirection() {
+		if(getnextStop()!=null) {
+			if(currentFloor.floorPostion<getnextStop().floorPostion && (this.running.code&RUNNING.UP.code)>0 )
+				return false;
+			if(currentFloor.floorPostion>getnextStop().floorPostion && (this.running.code&RUNNING.DOWN.code)>0 )
+				return false;
+		}
+		System.out.println(name+": 即将改变运行方向");
+		return true;
 	}
 
 	public void informPerson() {
-		 for(int i=0;i<persons.size();i++){
-			 if(persons.get(i)!=null)
+		for(int i=0;i<persons.size();i++){
+			if(persons.get(i)!=null)
 				if(persons.get(i).shouldOutElevator(this))
 					System.out.println(String.format(outPerson,name,currentFloor.floorPostion));
-		 }
-		 //persons.removeIf(person->{return person.getState()==STATE.INELEVATOR;});
+		}
+		//persons.removeIf(person->{return person.getState()==STATE.INELEVATOR;});
 	}
-	
-	
+
+
 	Elevator(Floor currentFloor,String name){
 		this.currentFloor=currentFloor;
 		this.running=RUNNING.STILL;
 		this.name=name;
 	}
-	
-	
+
+
 }
